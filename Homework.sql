06b0dd765f3a04fc5a544cdbf0532adeea2fa347
Yanin Swangrujithum <yaninswangrujithum@Yanins-MacBook-Pro.local>
2018-09-25 00:04:10 -0700

    continued adding work

--- Homework.sql
+++ Homework.sql
@@ -1,22 +1,27 @@
 -- 1a. Display the first and last names of all actors from the table actor.
 select first_name as 'First Name', last_name as 'Last Name'
 from actor;
+
 -- 1b. Display the first and last name of each actor in a single column in upper case letters. Name the column Actor Name.
 select concat(upper(first_name), ' ', upper(last_name)) as 'Actor Name'
 from actor;
+
 -- 2a. You need to find the ID number, first name, and last name of an actor, of whom you know only the first name, "Joe." What is one query would you use to obtain this information?
 select actor_id as ID, first_name as 'First Name', last_name as 'Last Name'
 from actor
 where first_name like 'Joe%';
+
 -- 2b. Find all actors whose last name contain the letters GEN:
 select first_name as 'First Name', last_name as 'Last Name'
 from actor 
 where last_name like '%G%E%N%';
+
 -- 2c. Find all actors whose last names contain the letters LI. This time, order the rows by last name and first name, in that order:
 select first_name as 'First Name', last_name as 'Last Name'
 from actor
 where last_name like '%LI%'
 order by last_name;
+
 -- 2d. Using IN, display the country_id and country columns of the following countries: Afghanistan, Bangladesh, and China:
 select country_id as 'ID', country as 'Country'
 from country
@@ -31,19 +36,23 @@ add column description BLOB;
 -- 3b. Very quickly you realize that entering descriptions for each actor is too much effort. Delete the description column.
 alter table actor
 drop column description;
+
 -- 4a. List the last names of actors, as well as how many actors have that last name.
 select last_name as 'Last Name', count(last_name) as 'Number of Occurance'
 from actor
 group by last_name;
+
 -- 4b. List last names of actors and the number of actors who have that last name, but only for names that are shared by at least two actors
 select last_name as 'Last Name', count(last_name) as 'Number of Occurance'
 from actor
 group by last_name
 having count(last_name) >= 2;
+
 -- 4c. The actor HARPO WILLIAMS was accidentally entered in the actor table as GROUCHO WILLIAMS. Write a query to fix the record.
 update actor
 set first_name = 'HARPO'
 where first_name = 'GROUCHO' and last_name = 'WILLIAMS';
+
 -- 4d. Perhaps we were too hasty in changing GROUCHO to HARPO. It turns out that GROUCHO was the correct name after all! 
 -- In a single query, if the first name of the actor is currently HARPO, change it to GROUCHO.
 rollback;
@@ -55,11 +64,39 @@ show create table address;
 
 -- 6a. Use JOIN to display the first and last names, as well as the address, of each staff member. Use the tables staff and address:
 
--- 6b. Use JOIN to display the total amount rung up by each staff member in August of 2005. Use tables staff and payment.
--- 6c. List each film and the number of actors who are listed for that film. Use tables film_actor and film. Use inner join.
--- 6d. How many copies of the film Hunchback Impossible exist in the inventory system?
--- 6e. Using the tables payment and customer and the JOIN command, list the total paid by each customer. List the customers alphabetically by last name:
+select staff.first_name as 'First Name', staff.last_name as 'Last Name', address.address as 'Address'
+from staff
+inner join address on staff.address_id = address.address_id;
 
+-- 6b. Use JOIN to display the total amount rung up by each staff member in August of 2005. Use tables staff and payment.
+select staff.first_name as 'First Name', staff.last_name as 'Last Name', sum(payment.amount) as 'Total Amount'
+from staff
+inner join payment on staff.staff_id = payment.staff_id
+where payment.payment_date like '2005-08%'
+group by staff.first_name, staff.last_name;
+
+-- 6c. List each film and the number of actors who are listed for that film. Use tables film_actor and film. Use inner join.
+select title as "Film Title", count(actor_id) as "Number of Actors"
+from film 
+inner join film_actor 
+on film.film_id = film_actor.film_id
+group by title;
+
+-- 6d. How many copies of the film Hunchback Impossible exist in the inventory system?
+select title as 'Film Title', count(inventory_id) as "Number of copies"
+from film
+inner join inventory
+on film.film_id = inventory.film_id
+where title = "Hunchback Impossible"
+group by title;
+
+-- 6e. Using the tables payment and customer and the JOIN command, list the total paid by each customer. List the customers alphabetically by last name:
+select first_name as "First Name", last_name as "Last Name", sum(amount) as "Total Paid"
+from customer c	
+inner join payment p	
+on p.customer_id = c.customer_id
+group by p.customer_id
+order by last_name;
 
 --     ![Total amount paid](Images/total_payment.png)
 
